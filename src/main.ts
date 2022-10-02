de63bdb713ae5fb52f035ac5e40a9be523c35b12
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-02 22:09:25 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('Chatty')
+    .setDescription('API for Chatty app')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
