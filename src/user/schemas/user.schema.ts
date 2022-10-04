0dd57d27cd96a093aaac57d4e3758ec2a50b87ac
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-04 10:41:07 +0200

    Add user entity

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,8 +1,101 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Schema({
   collection: 'User',
 })
-export class User {}
+export class User {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Auth', index: true })
+  authId: string;
+
+  @Prop({ type: String, default: '' })
+  profilePicture: string;
+
+  @Prop({
+    type: Number,
+    default: 0,
+  })
+  postsCount: number;
+
+  @Prop({
+    type: Number,
+    default: 0,
+  })
+  followersCount: number;
+
+  @Prop({
+    type: Number,
+    default: 0,
+  })
+  followingCount: number;
+
+  @Prop({ type: String, default: '' })
+  passwordResetToken: string;
+
+  @Prop({ type: Number })
+  passwordResetExpires: number;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
+  blocked: string[];
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
+  blockedBy: string[];
+
+  @Prop(
+    raw({
+      messages: { type: Boolean, default: true },
+      reactions: { type: Boolean, default: true },
+      comments: { type: Boolean, default: true },
+      follows: { type: Boolean, default: true },
+    }),
+  )
+  notifications: Record<string, boolean>;
+
+  @Prop(
+    raw({
+      facebook: { type: String, default: '' },
+      instagram: { type: String, default: '' },
+      twitter: { type: String, default: '' },
+      youtube: { type: String, default: '' },
+    }),
+  )
+  social: Record<string, string>;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  work: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  school: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  location: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  quote: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  bgImageVersion: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  bgImageId: string;
+}
 
 export const UserSchema = SchemaFactory.createForClass(User);
