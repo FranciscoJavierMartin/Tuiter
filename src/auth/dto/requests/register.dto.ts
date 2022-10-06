de6ea356a549a7bee81b3bfc11019c15007abfb1
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-06 22:14:40 +0200

    Register dto

--- src/auth/dto/requests/register.dto.ts
+++ src/auth/dto/requests/register.dto.ts
@@ -1,20 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({
+    description: 'User name',
+    example: 'johndoe',
+    minLength: 4,
+    maxLength: 8,
+    nullable: false,
+    uniqueItems: true,
+  })
   @IsString()
   @MinLength(4)
   @MaxLength(8)
   username: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'Johndoe1',
+    minLength: 4,
+    maxLength: 8,
+    nullable: false,
+  })
   @IsString()
   @MinLength(4)
   @MaxLength(8)
   password: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'john@doe.com',
+    nullable: false,
+    uniqueItems: true,
+  })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Avatar color',
+    example: 'blue',
+    nullable: false,
+  })
   @IsString()
   avatarColor: string;
 }
