011863f8124765798ccde0abd1d293e2a3cdc28c
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-06 22:40:49 +0200

    User controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { UserService } from '@/user/services/user.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
