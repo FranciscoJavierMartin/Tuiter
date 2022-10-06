a85daa72ce90db6c388f99464433b3b8a7ec535d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-06 22:21:05 +0200

    Register dto

--- src/auth/dto/responses/register.dto.ts
+++ src/auth/dto/responses/register.dto.ts
@@ -1,7 +1,22 @@
 import { UserDocument } from '@/user/models/user.model';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseRegisterDto {
+  @ApiProperty({
+    description: 'Message with relevant info',
+    nullable: false,
+  })
   message: string;
+
+  @ApiProperty({
+    description: 'User created',
+    nullable: false,
+  })
   user: UserDocument;
+
+  @ApiProperty({
+    description: 'JWT Token. Ready for use',
+    nullable: false,
+  })
   token: string;
 }
