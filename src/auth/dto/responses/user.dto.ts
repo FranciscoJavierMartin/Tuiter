23385f8a3cad058b0a2c2c580798fc499abf1f02
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-08 14:12:31 +0200

    Document user response

--- src/auth/dto/responses/user.dto.ts
+++ src/auth/dto/responses/user.dto.ts
@@ -8,25 +8,104 @@ export class ResponseUserDto {
     description: 'User id from "Users" collection',
   })
   _id: string | ObjectId;
+
+  @ApiProperty({
+    description: 'User id from "Auth" collection',
+  })
   authId: string | ObjectId;
+
+  @ApiProperty({
+    description: 'User name',
+  })
   username?: string;
+
+  @ApiProperty({
+    description: 'User email',
+  })
   email?: string;
-  password?: string;
+
+  @ApiProperty({
+    description: 'Avatar color',
+  })
   avatarColor?: string;
+
+  @ApiProperty({
+    description: 'User id',
+  })
   uId?: string;
+
+  @ApiProperty({
+    description: 'Number of posts created by user',
+  })
   postsCount: number;
+
+  @ApiProperty({
+    description: 'User job',
+  })
   work: string;
+
+  @ApiProperty({
+    description: 'User school',
+  })
   school: string;
+
+  @ApiProperty({
+    description: 'User quote',
+  })
   quote: string;
+
+  @ApiProperty({
+    description: 'User location',
+  })
   location: string;
+
+  @ApiProperty({
+    description: 'Users blocked by me',
+  })
   blocked: mongoose.Types.ObjectId[];
+
+  @ApiProperty({
+    description: 'Users that have blocked me',
+  })
   blockedBy: mongoose.Types.ObjectId[];
+
+  @ApiProperty({
+    description: 'Users that follow me',
+  })
   followersCount: number;
+
+  @ApiProperty({
+    description: 'Users that I follow',
+  })
   followingCount: number;
+
+  @ApiProperty({
+    description: 'User notifications',
+  })
   notifications: NotificationSettings;
+
+  @ApiProperty({
+    description: 'User social links',
+  })
   social: SocialLinks;
+
+  @ApiProperty({
+    description: 'Background image version (for cloudinary)',
+  })
   bgImageVersion: string;
+
+  @ApiProperty({
+    description: 'Background image id (for cloudinary)',
+  })
   bgImageId: string;
+
+  @ApiProperty({
+    description: 'Profile picture url',
+  })
   profilePicture: string;
+
+  @ApiProperty({
+    description: 'User created date',
+  })
   createdAt?: Date;
 }
