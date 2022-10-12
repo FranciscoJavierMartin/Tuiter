ec2a77a111d8c33c98b4181544b3d6442738e86d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-12 12:43:16 +0200

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import * as compression from 'compression';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
 
+  app.use(helmet());
   app.use(compression());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
