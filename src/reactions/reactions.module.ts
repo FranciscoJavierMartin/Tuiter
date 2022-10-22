d756ecc8fde255325dcad5bf092a405f15595eb0
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-22 21:59:39 +0200

    Add queue

--- src/reactions/reactions.module.ts
+++ src/reactions/reactions.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { ReactionsService } from '@/reactions/reactions.service';
 import { ReactionsController } from '@/reactions/reactions.controller';
 import { PassportModule } from '@nestjs/passport';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    BullModule.registerQueue({
+      name: 'reaction',
+      defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+          type: 'fixed',
+          delay: 5000,
+        },
+        removeOnComplete: true,
+      },
+    }),
+  ],
   controllers: [ReactionsController],
   providers: [ReactionsService],
 })
--- src/reactions/reactions.service.ts
+++ src/reactions/reactions.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CurrentUser } from '@/auth/interfaces/current-user.interface';
 import { AddReactionDto } from '@/reactions/dto/add-reaction.dto';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
 
 @Injectable()
 export class ReactionsService {
+  constructor(
+    @InjectQueue('reaction') private readonly reactionQueue: Queue<any>,
+  ) {}
+
   create(addReactionDto: AddReactionDto, user: CurrentUser) {
     return 'This action adds a new reaction';
   }
