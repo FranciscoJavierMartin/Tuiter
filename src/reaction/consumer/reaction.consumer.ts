1a3de1944aee4ccc2e34f71f1f81ac0e5fe017c0
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-23 17:24:55 +0200

    Fix try/catch

--- src/reaction/consumer/reaction.consumer.ts
+++ src/reaction/consumer/reaction.consumer.ts
@@ -21,29 +21,30 @@ export class ReactionConsumer extends BaseConsumer {
     job: Job<AddReactionJobData>,
     done: DoneCallback,
   ): Promise<void> {
-    const [reactionInDb, postUpdated] = await Promise.all([
-      await this.reactionRepository.saveReaction(
-        job.data.reaction,
-        job.data.previousFeeling,
-      ),
-      await this.postRepository.updatePostReactions(
-        job.data.reaction.postId,
-        job.data.reaction.feeling,
-        job.data.previousFeeling,
-      ),
-    ]);
-    job.progress(50);
-
-    await this.reactionCacheService.savePostReactionToCache(
-      job.data.reaction.postId,
-      { ...job.data.reaction, _id: reactionInDb.upsertedId },
-      postUpdated.reactions,
-      job.data.previousFeeling,
-    );
-
-    job.progress(100);
-    done(null, job.data);
     try {
+      const [reactionInDb, postUpdated] = await Promise.all([
+        await this.reactionRepository.saveReaction(
+          job.data.reaction,
+          job.data.previousFeeling,
+        ),
+        await this.postRepository.updatePostReactions(
+          job.data.reaction.postId,
+          job.data.reaction.feeling,
+          job.data.previousFeeling,
+        ),
+      ]);
+
+      job.progress(50);
+
+      await this.reactionCacheService.savePostReactionToCache(
+        job.data.reaction.postId,
+        { ...job.data.reaction, _id: reactionInDb.upsertedId },
+        postUpdated.reactions,
+        job.data.previousFeeling,
+      );
+
+      job.progress(100);
+      done(null, job.data);
     } catch (error) {
       this.logger.error(error);
       done(error as Error);
