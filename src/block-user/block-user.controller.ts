0c5fb903195b27d15127b15160db1f3182c3e302
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 22:48:48 +0100

    Get follower id

--- src/block-user/block-user.controller.ts
+++ src/block-user/block-user.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Put } from '@nestjs/common';
+import { Controller, Param, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
+import { ID } from '@/shared/interfaces/types';
 import { BlockUserService } from '@/block-user/block-user.service';
 
-@Controller('block-user')
+@ApiTags('Block user')
+@Controller('user')
 export class BlockUserController {
   constructor(private readonly blockUserService: BlockUserService) {}
 
-  @Put()
-  block() {
-    return 'block user';
+  @Put('block/:followerId')
+  public block(@Param('followerId', ValidateIdPipe) followerId: ID) {
+    return followerId;
   }
 }
