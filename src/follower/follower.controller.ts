07b9d85651c2d29e8cf6d8abff6aa33076c2d0c8
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 11:02:11 +0100

    Add follow controller

--- src/follower/follower.controller.ts
+++ src/follower/follower.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FollowerService } from './follower.service';
 
 @ApiTags('Follow')
-@Controller('user/follow')
+@Controller('user')
 export class FollowerController {
   constructor(private readonly followerService: FollowerService) {}
+
+  @Put('follow/:followeeId')
+  public async follow(@Param('followeeId') followeeId: string) {
+    return followeeId;
+  }
 }
