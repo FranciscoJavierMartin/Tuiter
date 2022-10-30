31a18161c683ae923197d6cbc41a1090e6ebd093
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 11:40:16 +0100

    Reject request about itself

--- src/follower/follower.controller.ts
+++ src/follower/follower.controller.ts
@@ -5,6 +5,7 @@ import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
 import { FollowerService } from '@/follower/follower.service';
 import { NotMySelfGuard } from './guards/not-my-self.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '@/auth/decorators/get-user.decorator';
 
 @ApiTags('Follow')
 @Controller('user')
@@ -13,7 +14,10 @@ export class FollowerController {
 
   @Put('follow/:followeeId')
   @UseGuards(AuthGuard(), NotMySelfGuard)
-  public async follow(@Param('followeeId', ValidateIdPipe) followeeId: ID) {
-    return followeeId;
+  public async follow(
+    @Param('followeeId', ValidateIdPipe) followeeId: ID,
+    @GetUser() user: any,
+  ) {
+    return { followeeId, user };
   }
 }
--- src/follower/guards/not-my-self.guard.ts
+++ src/follower/guards/not-my-self.guard.ts
@@ -1,11 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class NotMySelfGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  public canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const followeeId = request.params.followeeId;
+    const followerId = request.params.followerId;
+    const userId = request.user.userId;
+
+    return !(
+      (followeeId && followeeId === userId) ||
+      (followerId && followerId === userId)
+    );
   }
 }
