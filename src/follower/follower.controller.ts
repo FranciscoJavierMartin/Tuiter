bbfb977bad0938ccbcd42e620d90600356221f78
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 11:16:12 +0100

    Add not my self guard

--- src/follower/follower.controller.ts
+++ src/follower/follower.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Param, Put } from '@nestjs/common';
+import { Controller, Param, Put, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ID } from '@/shared/interfaces/types';
 import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
 import { FollowerService } from '@/follower/follower.service';
+import { NotMySelfGuard } from './guards/not-my-self.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Follow')
 @Controller('user')
@@ -10,6 +12,7 @@ export class FollowerController {
   constructor(private readonly followerService: FollowerService) {}
 
   @Put('follow/:followeeId')
+  @UseGuards(AuthGuard(), NotMySelfGuard)
   public async follow(@Param('followeeId', ValidateIdPipe) followeeId: ID) {
     return followeeId;
   }
--- src/follower/follower.module.ts
+++ src/follower/follower.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { FollowerService } from './follower.service';
-import { FollowerController } from './follower.controller';
+import { PassportModule } from '@nestjs/passport';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FollowerService } from '@/follower/follower.service';
+import { FollowerController } from '@/follower/follower.controller';
+import { Follower, FollowerSchema } from '@/follower/models/follower.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Follower.name, schema: FollowerSchema },
+    ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [FollowerController],
-  providers: [FollowerService]
+  providers: [FollowerService],
 })
 export class FollowerModule {}
