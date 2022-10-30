a12786dec8e409ae1c27ebd57cdb937bcacb7a8c
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-10-30 22:52:03 +0100

    Add auth guard

--- src/block-user/block-user.controller.ts
+++ src/block-user/block-user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Param, Put } from '@nestjs/common';
+import { Controller, Param, Put, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
 import { ID } from '@/shared/interfaces/types';
 import { BlockUserService } from '@/block-user/block-user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { NotMySelfGuard } from '@/follower/guards/not-my-self.guard';
 
 @ApiTags('Block user')
 @Controller('user')
@@ -10,6 +12,7 @@ export class BlockUserController {
   constructor(private readonly blockUserService: BlockUserService) {}
 
   @Put('block/:followerId')
+  @UseGuards(AuthGuard(), NotMySelfGuard)
   public block(@Param('followerId', ValidateIdPipe) followerId: ID) {
     return followerId;
   }
--- src/block-user/block-user.module.ts
+++ src/block-user/block-user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlockUserService } from '@/block-user/block-user.service';
 import { BlockUserController } from '@/block-user/block-user.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [BlockUserController],
   providers: [BlockUserService],
 })
