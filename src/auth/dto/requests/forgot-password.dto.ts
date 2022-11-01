8c96f5f57f6cb7773ad52090511666b6bd27b10d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-01 21:01:28 +0100

    Forgot password Api descriptor

--- src/auth/dto/requests/forgot-password.dto.ts
+++ src/auth/dto/requests/forgot-password.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class ForgotPasswordDto {
+  @ApiProperty({
+    description: 'User email',
+    required: true,
+    uniqueItems: true,
+  })
   @IsString()
   @IsEmail()
   email: string;
