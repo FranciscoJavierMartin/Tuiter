6087b240c2b0e2fcfe74661a12c5b0f749cbfd64
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-03 22:35:34 +0100

    Add notification model

---
+++ src/notification/interfaces/notification.interface.ts
@@ -0,0 +1,21 @@
+// import { ID } from '@/shared/interfaces/types';
+
+export enum NotificationType {
+  Comment = 'comment',
+}
+
+// export interface NotificationBody {
+//   userTo: ID;
+//   userFrom: ID;
+//   message: string;
+//   notificationType: NotificationType;
+//   entityId: ID;
+//   createdItemId: ID;
+//   createdAt: Date;
+//   comment: string;
+//   reaction: string;
+//   post: string;
+//   imgId: string;
+//   imgVersion: string;
+//   gifUrl: string;
+// }
---
+++ src/notification/models/notification.model.ts
@@ -0,0 +1,81 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+import { NotificationType } from '@/notification/interfaces/notification.interface';
+
+@Schema({
+  collection: 'Notification',
+})
+export class Notification {
+  _id: ObjectId;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+  })
+  userTo: ObjectId;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  })
+  userFrom: ObjectId;
+
+  @Prop({ type: Boolean, default: false })
+  read: boolean;
+
+  @Prop({ type: String, default: '' })
+  message: string;
+
+  @Prop({ type: String, enum: NotificationType })
+  notificationType: NotificationType;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId })
+  entityId: ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId })
+  createdItemId: ObjectId;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  comment: string;
+
+  // TODO: Check if could use another type
+  @Prop({
+    type: String,
+    default: '',
+  })
+  reaction: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  post: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  imgId: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  imgVersion: string;
+
+  @Prop({
+    type: String,
+    default: '',
+  })
+  gifUrl: string;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date = new Date();
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
