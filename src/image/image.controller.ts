18c86e5aae7bfeadc668d6e7c46790f9eb2254ae
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-07 20:02:13 +0100

    Upload profile picture

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Patch } from '@nestjs/common';
 import { ImageService } from '@/image/image.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Image')
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
+
+  @Patch('profile')
+  public async uploadProfilePicture() {}
 }
