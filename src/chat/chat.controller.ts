3b40a91fdd87fd11f240e85228d4e1838278b2c9
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-16 19:53:23 +0100

    Add message basic controller

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ChatService } from './chat.service';
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  @Post('message')
+  public async addMessage() {
+    return 'addMessage';
+  }
 }
