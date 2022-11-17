99ea893c21d4a4629f4188a3655c1a1cb7685a0d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-17 20:09:20 +0100

    Get talkers

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,11 +3,13 @@ import { ChatService } from '@/chat/chat.service';
 import { ChatController } from '@/chat/chat.controller';
 import { PassportModule } from '@nestjs/passport';
 import { BlockUserModule } from '@/block-user/block-user.module';
+import { UserModule } from '@/user/user.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     BlockUserModule,
+    UserModule,
   ],
   controllers: [ChatController],
   providers: [ChatService],
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -4,18 +4,31 @@ import { CurrentUser } from '@/auth/interfaces/current-user.interface';
 import { AddMessageDto } from '@/chat/dto/requests/add-message.dto';
 import { MessageData } from '@/chat/interfaces/chat.interface';
 import { ID } from '@/shared/interfaces/types';
+import { UploaderService } from '@/shared/services/uploader.service';
+import { UserCacheService } from '@/user/services/user.cache.service';
 
 @Injectable()
 export class ChatService {
+  constructor(
+    private readonly uploaderService: UploaderService,
+    private readonly userCacheService: UserCacheService,
+  ) {}
+
   public async addMessage(
     receiverId: ID,
     addMessageDto: AddMessageDto,
     currentUser: CurrentUser,
     image?: Express.Multer.File,
   ): Promise<void> {
-    // TODO: Add upload image feature
-    // const messageData: MessageData = {
-    //   _id: new ObjectId(),
-    // };
+    const [sender, receiver] = await Promise.all([
+      await this.userCacheService.getUserFromCache(currentUser.userId),
+      await this.userCacheService.getUserFromCache(receiverId),
+    ]);
+
+    this.emitSocketIOEvent(undefined);
+  }
+
+  private emitSocketIOEvent(data: any): void {
+    // TODO: Emit 'message received' and 'chat list'
   }
 }
