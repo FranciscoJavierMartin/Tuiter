7528b7c7a63f7c7eee833bb6762ce701e8b7fc45
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-26 12:45:36 +0100

    Add more info to body

--- src/user/dto/requests/user-info.dto.ts
+++ src/user/dto/requests/user-info.dto.ts
@@ -10,7 +10,6 @@ import {
 export class UserInfoDto {
   @ApiProperty({
     description: 'Quote to be displayed in user profile page',
-    nullable: true,
     required: false,
   })
   @ValidateIf((o) => !!o.quote)
@@ -19,4 +18,37 @@ export class UserInfoDto {
   @MinLength(2)
   @MaxLength(50)
   quote?: string;
+
+  @ApiProperty({
+    description: 'User work',
+    required: false,
+  })
+  @ValidateIf((o) => !!o.work)
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(30)
+  work?: string;
+
+  @ApiProperty({
+    description: 'User work',
+    required: false,
+  })
+  @ValidateIf((o) => !!o.school)
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  school?: string;
+
+  @ApiProperty({
+    description: 'User location',
+    required: false,
+  })
+  @ValidateIf((o) => !!o.location)
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  location?: string;
 }
