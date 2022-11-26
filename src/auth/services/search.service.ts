d5073a63936cddf29d68bbfeced49b8e60d162ec
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-26 12:07:18 +0100

    Search service docs

--- src/auth/services/search.service.ts
+++ src/auth/services/search.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { AuthRepository } from '@/auth/repositories/auth.repository';
 import { SearchUserDto } from '@/user/dto/responses/search-user.dto';
+import { AuthRepository } from '@/auth/repositories/auth.repository';
 
 @Injectable()
 export class SearchService {
   constructor(private readonly authRepository: AuthRepository) {}
 
+  /**
+   * Search users with given username match
+   * @param regexp Regular expression to match username
+   * @returns User list that match regexp
+   */
   public async searchUsers(regexp: RegExp): Promise<SearchUserDto[]> {
     return this.authRepository.searchUsers(regexp);
   }
