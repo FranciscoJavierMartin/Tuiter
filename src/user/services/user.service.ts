c2094dc7926db086ce6521a358b544594539c39d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-27 22:25:05 +0100

    Add jsdoc param

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -88,6 +88,7 @@ export class UserService {
 
   /**
    * Get random users from data source
+   * @param userId User id to exclude
    * @returns Random users
    */
   public async getRandomUsers(userId?: string): Promise<UserDto[]> {
