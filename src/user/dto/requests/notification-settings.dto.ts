c041886affa09a204a743bf13da990732ae110a7
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-27 10:27:20 +0100

    Add more properties for notification settings body

--- src/user/dto/requests/notification-settings.dto.ts
+++ src/user/dto/requests/notification-settings.dto.ts
@@ -10,4 +10,31 @@ export class NotificationSettingsDto {
   @IsBoolean()
   @IsOptional()
   messages?: boolean;
+
+  @ApiPropertyOptional({
+    name: 'reactions',
+    description: 'Enable or disable email notifications for reactions',
+    example: true,
+  })
+  @IsBoolean()
+  @IsOptional()
+  reactions?: boolean;
+
+  @ApiPropertyOptional({
+    name: 'comments',
+    description: 'Enable or disable email notifications for comments',
+    example: true,
+  })
+  @IsBoolean()
+  @IsOptional()
+  comments?: boolean;
+
+  @ApiPropertyOptional({
+    name: 'follows',
+    description: 'Enable or disable email notifications for follows',
+    example: true,
+  })
+  @IsBoolean()
+  @IsOptional()
+  follows?: boolean;
 }
