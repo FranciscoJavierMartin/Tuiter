277468f1d20d4fa7e21f54de4bc75bdf7bc7baf2
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-11-28 23:09:03 +0100

    Instance route

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('health')
 export class HealthController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get()
-  public async health(): Promise<string> {
+  public health(): string {
     return `Health: Server instance is healthy with process id ${
       process.pid
-    } on ${new Date().toLocaleDateString(undefined, {
+    } on ${this.getCurrentDate()}`;
+  }
+
+  @Get('env')
+  public env(): string {
+    return `This is the ${process.env.NODE_ENV} environment.`;
+  }
+
+  @Get('instance')
+  public async instance(): Promise<string> {
+    const response = await fetch(this.configService.get('EC2_URL'));
+    const data = await response.json();
+
+    return `Server is running on EC2 instance with id ${data} and process id ${
+      process.pid
+    } on ${this.getCurrentDate()}`;
+  }
+
+  private getCurrentDate(): string {
+    return new Date().toLocaleDateString(undefined, {
       day: '2-digit',
       weekday: 'short',
       month: 'short',
@@ -14,11 +36,6 @@ export class HealthController {
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
-    })}`;
-  }
-
-  @Get('env')
-  public async env(): Promise<string> {
-    return `This is the ${process.env.NODE_ENV} environment.`;
+    });
   }
 }
