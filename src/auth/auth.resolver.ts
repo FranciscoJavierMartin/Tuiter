e36d28636edc3be480b69e14ed21e47b8bdf68c6
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-19 13:14:01 +0100

    Create register

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { AuthService } from '@/auth/services/auth.service';
 import { LoginDto } from '@/auth/dto/requests/login.dto';
 import { ResponseRegisterDto } from '@/auth/dto/responses/register.dto';
@@ -10,5 +10,10 @@ export class AuthResolver {
   @Query(() => ResponseRegisterDto, { name: 'login' })
   public async login(@Args() loginDto: LoginDto): Promise<ResponseRegisterDto> {
     return this.authService.login(loginDto);
+  }
+
+  @Mutation(() => String, { name: 'register' })
+  public async register(): Promise<string> {
+    return 'Register';
   }
 }
