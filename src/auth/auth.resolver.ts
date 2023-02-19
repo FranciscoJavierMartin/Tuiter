e0f8e0b43e2cbf05a245f261646c0d995a4d75a5
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-19 12:30:31 +0100

    Login via mutation

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,9 +1,14 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { AuthService } from '@/auth/services/auth.service';
+import { LoginDto } from '@/auth/dto/requests/login.dto';
+import { ResponseRegisterDto } from '@/auth/dto/responses/register.dto';
 
 @Resolver()
 export class AuthResolver {
-  @Query(() => String)
-  public async login(): Promise<string> {
-    return 'Hello world from login';
+  constructor(private readonly authService: AuthService) {}
+
+  @Query(() => ResponseRegisterDto, { name: 'login' })
+  public async login(@Args() loginDto: LoginDto): Promise<ResponseRegisterDto> {
+    return this.authService.login(loginDto);
   }
 }
