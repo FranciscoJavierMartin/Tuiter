93bfaa30d2d77e03e12374d715ceca5ce7707542
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-20 22:47:25 +0100

    Add todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { ImageModule } from '@/image/image.module';
 import { ChatModule } from '@/chat/chat.module';
 import { HealthModule } from '@/health/health.module';
 
+// TODO: Continue with GraphQL support
 // TODO: If the result list from Redis is empty, then search in DB
 // TODO: For "workers" request use HTTP Accepted 202
 // TODO: Add API KEY to each request. For avoid third party request
