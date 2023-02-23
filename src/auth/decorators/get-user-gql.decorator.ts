60c1153b5dcc243c217fadf7af02bd0c1cabe979
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-23 20:54:32 +0100

    Get user in decorator

--- src/auth/decorators/get-user-gql.decorator.ts
+++ src/auth/decorators/get-user-gql.decorator.ts
@@ -1,12 +1,32 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ObjectId } from 'mongodb';
+import { ID } from '@/shared/interfaces/types';
+import { CurrentUser } from '@/auth/interfaces/current-user.interface';
 
 export const GetUserGql = createParamDecorator(
   (field, context: ExecutionContext) => {
     const ctx: GqlExecutionContext = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
     const user = req.user;
-    console.log('Decorator', user);
-    return 'Hello';
+    let res: string | ID | CurrentUser;
+
+    if (!user) {
+      throw new InternalServerErrorException('User not found (request)');
+    }
+
+    if (field === 'userId') {
+      res = new ObjectId(user[field]);
+    } else if (field) {
+      res = user[field];
+    } else {
+      res = user;
+    }
+
+    return res;
   },
 );
