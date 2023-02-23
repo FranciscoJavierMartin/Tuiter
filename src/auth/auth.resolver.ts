dd40d81e92733f79d79ac82ef0a8921016890f61
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-23 22:48:09 +0100

    Create mutation

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,5 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { UserDto } from '@/user/dto/responses/user.dto';
 import { AuthService } from '@/auth/services/auth.service';
 import { LoginDto } from '@/auth/dto/requests/login.dto';
 import { RegisterDto } from '@/auth/dto/requests/register.dto';
@@ -7,7 +8,6 @@ import { ResponseRegisterDto } from '@/auth/dto/responses/register.dto';
 import { CurrentUser } from '@/auth/interfaces/current-user.interface';
 import { GetUserGql } from '@/auth/decorators/get-user-gql.decorator';
 import { GqlAuthGuard } from '@/auth/guards/gql-auth.guard';
-import { UserDto } from '@/user/dto/responses/user.dto';
 
 @Resolver()
 export class AuthResolver {
@@ -33,12 +33,23 @@ export class AuthResolver {
     return this.authService.create(registerDto);
   }
 
-  @Query(() => UserDto, { name: 'currentUser', description: 'User info' })
+  @Query(() => UserDto, {
+    name: 'currentUser',
+    description: 'Get current user info',
+  })
   @UseGuards(GqlAuthGuard)
   public async getCurrentUser(
     @GetUserGql() user: CurrentUser,
   ): Promise<UserDto> {
     const userFromServer = await this.authService.getUser(user.userId);
     return new UserDto(userFromServer);
+  }
+
+  @Mutation(() => String, {
+    name: 'forgotPassword',
+    description: 'Send reset password email',
+  })
+  public async forgotPassword(): Promise<string> {
+    return 'Hello';
   }
 }
--- src/auth/dto/responses/info-message.dto.ts
+++ src/auth/dto/responses/info-message.dto.ts
@@ -1,8 +1,10 @@
+import { Field } from '@nestjs/graphql';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class InfoMessageDto {
   @ApiProperty({
     description: 'Message with info',
   })
+  @Field(() => String)
   message: string;
 }
