05e5c89592219533f5435c21afdb9607f46c5a14
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-26 19:29:37 +0100

    Get current user

--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -3,7 +3,9 @@ import { Args, ID, Mutation, Resolver } from '@nestjs/graphql';
 import { InfoMessageDto } from '@/shared/dto/responses/info-message.dto';
 import { ID as Id } from '@/shared/interfaces/types';
 import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
+import { GetUserGql } from '@/auth/decorators/get-user-gql.decorator';
 import { GqlAuthGuard } from '@/auth/guards/gql-auth.guard';
+import { CurrentUser } from '@/auth/interfaces/current-user.interface';
 import { ChatService } from '@/chat/chat.service';
 
 @Resolver()
@@ -17,8 +19,9 @@ export class ChatResolver {
   })
   public async addMessage(
     @Args('receiverId', { type: () => ID }, ValidateIdPipe) receiverId: Id,
+    @GetUserGql() currentUser: CurrentUser,
   ): Promise<InfoMessageDto> {
-    console.log(receiverId);
+    console.log(currentUser);
     return {
       message: 'Message added',
     };
