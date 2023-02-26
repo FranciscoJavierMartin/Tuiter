b197ea06fd0bee0f43afcdc7d0967e6783bd4301
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-02-26 19:20:31 +0100

    Get param

--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -1,8 +1,10 @@
 import { UseGuards } from '@nestjs/common';
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, ID, Mutation, Resolver } from '@nestjs/graphql';
+import { InfoMessageDto } from '@/shared/dto/responses/info-message.dto';
+import { ID as Id } from '@/shared/interfaces/types';
+import { ValidateIdPipe } from '@/shared/pipes/validate-id.pipe';
 import { GqlAuthGuard } from '@/auth/guards/gql-auth.guard';
 import { ChatService } from '@/chat/chat.service';
-import { InfoMessageDto } from '@/shared/dto/responses/info-message.dto';
 
 @Resolver()
 @UseGuards(GqlAuthGuard)
@@ -13,7 +15,10 @@ export class ChatResolver {
     name: 'addMessage',
     description: 'Add a message',
   })
-  public async addMessage(): Promise<InfoMessageDto> {
+  public async addMessage(
+    @Args('receiverId', { type: () => ID }, ValidateIdPipe) receiverId: Id,
+  ): Promise<InfoMessageDto> {
+    console.log(receiverId);
     return {
       message: 'Message added',
     };
